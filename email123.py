c4c7da7917f8bc927cb50fe37349c900bc363ff2
ArielYat <82038633+ArielYat@users.noreply.github.com>
2023-04-01 20:55:56 +0300

    added stable diffusion domain

--- email123.py
+++ email123.py
@@ -1,21 +1,28 @@
 import smtplib
 import time
-import subprocess
 import logging as log
 import os
 from dotenv import load_dotenv
 import random
-import re
 import socket
+import requests
 
 GREETING_ARRAY = ["Hi", "Good Morning", "Have a nice day", "Hello", "Hey, how are you?"]
 
 time.sleep(100)
 try:
 	server = smtplib.SMTP('smtp-mail.outlook.com', 587)
-	test = subprocess.check_output("tmux capture-pane -pS Mysession", shell=True)
-	t = re.findall(r"\\nForwarding.+tcp:\/\/(.+)-> localhost:22\\n", str(test))[0].strip()
-	s = re.findall(r"\\nForwarding.+tcp:\/\/(.+)-> localhost:25565\\n", str(test))[0].strip()
+	serverDomain = ''
+	minecraftServerDomain = ''
+	stableServerDomain = ''
+	for site in requests.get("http://localhost:4040/api/tunnels/").json()['tunnels']:
+		if 'first' in site['uri']:
+			serverDomain = site['public_url']
+		elif 'third' in site['uri']:
+			minecraftServerDomain = site['public_url']
+		elif 'second' in site['uri']:
+			stableServerDomain = site['public_url']
+
 	server.starttls()
 	load_dotenv()
 	USER = os.getenv('USER')
@@ -25,11 +32,12 @@ try:
 	sender = USER
 	receivers = os.getenv('RECEIVERS').split()
 	server.sendmail(sender, receivers, f"Subject: Good morning\n\n" +
-	                f"Hello this is the domain for the server {t}\n\nminecraft server:{s}\n\n" +
+	                f"Hello this is the domain for the server {serverDomain}\n\nminecraft server:{minecraftServerDomain}" +
+					f"\n\nstable diffusion domain: {stableServerDomain}\n\n" +
 	                random.choice(GREETING_ARRAY))
 	log.basicConfig(filename='/home/moshe/startup/log.txt', level=log.DEBUG, format='%(asctime)s %(message)s')
 	log.info('Email sent')
 	server.quit()
 except Exception as e  :
-    print(str(e))
-    socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("127.0.0.1", 6958))
+	print(str(e))
+	socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("127.0.0.1", 6958))
